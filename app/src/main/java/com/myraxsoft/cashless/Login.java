bf5398f4cd307d3290d75dd839be33ceeba43a51
shahrul <shahrul@shahruls-MacBook-Air.local>
2019-02-28 20:53:37 +0800

    publish key v2

--- app/src/main/java/com/myraxsoft/cashless/Login.java
+++ app/src/main/java/com/myraxsoft/cashless/Login.java
@@ -1,7 +1,10 @@
 package com.myraxsoft.cashless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        Button btnToRegister = (Button) findViewById(R.id.btnToRegister);
+        btnToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToRegister();
+            }
+        });
+    }
+
+    public void goToRegister(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
